a120d0bf70b420e1edad6c3e94b64b86e959a925
Qu31R0zZ <allan754@outlook.com>
2025-04-11 10:46:13 -0300

    Aula 56

---
+++ Aula 55 - Leitura da serial/src/main.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include "BluetoothSerial.h"
+#define pinLed 2
+
+String palavra = "";
+//*     ********instancias de objeto*********
+
+BluetoothSerial SerialBT;
+
+void setup()
+{
+  Serial.begin(9600);
+  SerialBT.begin("espAllan");
+  SerialBT.println("Digite algo no monitor Serial:.");
+  pinMode(pinLed, OUTPUT);
+}
+
+void loop()
+{
+  while (SerialBT.available())
+  {
+    char caracter = SerialBT.read();
+    if (caracter == '\n')
+    {
+      SerialBT.println(palavra);
+      if (palavra.equals("liga")) //*equals serve para saber se a string e igual a um apalavra
+      {
+        digitalWrite(pinLed, HIGH);
+        SerialBT.println("LED ligado");
+      }
+      else if (palavra.equals("desliga"))
+      {
+        digitalWrite(pinLed, LOW);
+        SerialBT.println("LED desligado");
+      }
+      else
+      {
+        SerialBT.println("comando nao reconhecido");
+      }
+      palavra = "";
+    }
+    else if (caracter != '\r')
+    {
+      palavra += caracter;
+    }
+  }
+}
